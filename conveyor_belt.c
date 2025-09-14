4cb4664e9ed33533d103075faf05c2c99d6606f7
pellecajus <s255327@dtu.dk>
2025-09-14 16:19:17 +0200

    Add files via upload

---
+++ conveyor_belt.c
@@ -0,0 +1,21 @@
+// Øvelse omkring bærebelter
+#include <studio.h>
+
+int main() {
+    int motor;
+    float taske_vægt;
+    
+    // promt for antalet motorer
+    printf("hvor mange motorer bærer tasken?");
+    scanf(%& , &motor);
+
+    // promt for vægten på tasken 
+    printf("hvor meget din taske, min ven?");
+    scanf("%f" , &taske_vægt");
+
+    // Udregning af vægt per motor
+    float vægt_per_motor = taske_vægt / motor; 
+
+    // check om beltet kan bære vægten
+    if
+}
